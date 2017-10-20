f2484b83135483ac4006087d0098773d4f3c5926
joysonmoses <joyson2121@gmail.com>
2017-10-20 19:17:25 +0530

    added the first file

---
+++ delete_manager.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+include_once('connect_db.php');
+if(isset($_SESSION['username'])){
+$id=$_SESSION['admin_id'];
+$user=$_SESSION['username'];
+}else{
+header("location:http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/index.php");
+exit();
+}
+$id=$_GET[manager_id];
+$sql="delete from manager where manager_id='$id'";
+mysql_query($sql);
+header("location:admin_manager.php");
+?>
+
+
